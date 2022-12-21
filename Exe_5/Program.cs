dd744ac7f63477c54ba70f7e9789859dd9840421
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-12-21 16:48:40 +0700

    add method main

--- Exe_5/Program.cs
+++ Exe_5/Program.cs
@@ -61,4 +61,46 @@ namespace Exe_5
                 Console.WriteLine(Display.name);
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue qe = new Queue();
+            char ch;
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1.Menambahkan Data ke dalam List");
+                    Console.WriteLine("2.Menghapus  Data dari dalam List");
+                    Console.WriteLine("3.Melihat semua data  di dalam List");
+                    Console.WriteLine("4.Exit");
+                    Console.WriteLine("Masukkan pilihan anda (1 - 4: ");
+                    ch = Convert.ToChar(Console.ReadKey());
+                    switch (ch)
+                    {
+                        case '1':
+                            qe.Insert();
+                            break;
+                        case '2':
+                            qe.delete();
+                            break;
+                        case '3':
+                            qe.Display();
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            Console.WriteLine("Wrong Option");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nCheck for the value entered ");
+                }
+            }
+        }
+    }
 }
